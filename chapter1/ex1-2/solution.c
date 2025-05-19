ea6736e56ef262741542d390de317cc6bee15254
3akare <bakaredavid007@gmail.com>
2025-05-19 19:11:07 +0100

    feat [ex1-2]: complete coding exercise

--- chapter1/ex1-2/solution.c
+++ chapter1/ex1-2/solution.c
@@ -1,10 +1,18 @@
 #include <stdio.h>
 
-int main()
+/**
+ * @brief Experiments with escape sequences.
+ * 
+ * @return 0
+ * */
+
+int main(void)
 {
-    printf("hello, world\n");    /* \n is valid */
-    printf("hello, world\\c is invalid\n");    /* \c is invalid */
-    printf("hello, world\\7 is valid\n");    /* \7 is valid (bell) */
-    printf("hello, world\\? is valid\n");    /* \? is valid */
-    printf("hello, world\\x is invalid\n");    /* \x is invalid */
-} 
+    // printf("Trying \\x: Hello\xWorld!\n"); /* (observation: The compiler produced an error: "\x used with no following hex digits"). RESULT: Error */
+    printf("Trying \\y: Hello\yworld!\n"); /* (observation: Compiler warning "unknown escape sequence: '\y'"). RESULT: Trying \y: Helloyworld! */
+    printf("Trying \\z: Hello\zworld!\n"); /* (observation: Compiler warning "unknown escape sequence: '\z'"). RESULT: Trying \z: Hellozworld! */
+    printf("Trying \\0: Hello\0world!\n"); /* (observation: \0 is the null terminator, marking the end of a string. The printf function stops processing the string at this point, ignoring characters that come after '\0'). RESULT: Trying \0: Hello */
+    printf("Trying \\1: Hello\1world!\n"); /* RESULT: Trying \1: Helloworld! */
+    printf("Trying \\2: Hello\2world!\n"); /* RESULT: Trying \2: Helloworld! */
+    return 0;
+}
